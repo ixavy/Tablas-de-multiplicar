7af01d6e200c760415fbd521d61e9d287f19868c
Javier <development@ixavy.com>
2016-05-31 14:44:37 +0200

    Primera version

--- Tablas de multiplicar/ViewController.swift
+++ Tablas de multiplicar/ViewController.swift
@@ -8,11 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    var sliderValue:Int = 0
+    
+    @IBOutlet weak var slideOutlet: UISlider!
+    
+    @IBOutlet weak var table: UITableView!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func slider(sender: AnyObject) {
+
+        sliderValue = Int(slideOutlet.value * 10 )
+        label.text = String(sliderValue)
+        table.reloadData()
+    }
+    
+    internal func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    internal func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = String(sliderValue * (indexPath.row + 1))
+        cell.detailTextLabel?.text = String(indexPath.row + 1
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        table.delegate = self
+        
+
     }
 
     override func didReceiveMemoryWarning() {
